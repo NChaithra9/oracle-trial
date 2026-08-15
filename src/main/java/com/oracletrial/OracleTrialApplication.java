64a8f5f49bebf14204263a428d1e469da586e66f
NChaithra9 <n.chaithra@calfus.com>
2026-08-15 20:23:33 +0530

    Initialize Oracle Trial backend with health API

---
+++ src/main/java/com/oracletrial/OracleTrialApplication.java
@@ -0,0 +1,13 @@
+package com.oracletrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleTrialApplication.class, args);
+	}
+
+}
